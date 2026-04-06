2ccbcfae1780e5e56631e4604838cf5d5ad1f970
johnverz22 <johnny.verzola@lorma.edu>
2026-04-07 04:32:13 +0800

    merge conflict

---
+++ mobile/passenger/lib/features/home/models/nearby_driver.dart
@@ -0,0 +1,46 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+enum VehicleType { motorcycle, car, tricycle }
+
+class NearbyDriver {
+  final String id;
+  final String name;
+  final LatLng location;
+  final double heading;
+  final VehicleType vehicleType;
+
+  NearbyDriver({
+    required this.id,
+    required this.name,
+    required this.location,
+    required this.heading,
+    required this.vehicleType,
+  });
+
+  factory NearbyDriver.fromJson(Map<String, dynamic> json) {
+    final locJson = json['location'] as Map<String, dynamic>;
+    return NearbyDriver(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      location: LatLng(
+        (locJson['lat'] as num).toDouble(),
+        (locJson['lng'] as num).toDouble(),
+      ),
+      heading: (json['heading'] as num?)?.toDouble() ?? 0.0,
+      vehicleType: _parseVehicleType(json['vehicle_type'] as String),
+    );
+  }
+
+  static VehicleType _parseVehicleType(String type) {
+    switch (type) {
+      case 'motorcycle':
+        return VehicleType.motorcycle;
+      case 'car':
+        return VehicleType.car;
+      case 'tricycle':
+        return VehicleType.tricycle;
+      default:
+        return VehicleType.motorcycle;
+    }
+  }
+}
